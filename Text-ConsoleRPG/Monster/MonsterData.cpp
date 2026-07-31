015f77d3dbe15b1b3410b7a6afd1f45282f1f60b
SeongEun-Kwack <144892153+SeongEun-Kwack@users.noreply.github.com>
2026-07-31 16:39:30 +0900

    add boss dropitem -> license

--- Text-ConsoleRPG/Monster/MonsterData.cpp
+++ Text-ConsoleRPG/Monster/MonsterData.cpp
@@ -8,25 +8,26 @@ vector<MonsterData> MonsterList() {
 
                         // group, name,          hp, atk, def, spd, dropitenname,    category, price, exp, isboss
         // Slime
-        { MonsterGroup::Slime,  "Small Slime",   80,  15, 5, 0,  "Slime Jelly",     "parts",  10, 20,  false },
-        { MonsterGroup::Slime,  "Slimes",        120, 20, 8, 0,  "Slime Jelly",     "parts",  10, 30,  false },
-        { MonsterGroup::Slime,  "Fire Slimes",   150, 25, 8, 0, "Slime Jelly",      "parts",  10, 40,  false },
-        { MonsterGroup::Slime,  "Dark Slimes",   180, 30, 10, 0, "Slime Jelly",      "parts",  10, 50,  false },
-        { MonsterGroup::Slime,  "Slime King",    300, 40, 15, 0, "Slime Jelly",     "parts",  10, 100, true  },
+        { MonsterGroup::Slime,  "작은 슬라임",   80,  15, 5, 0,  "슬라임 부산물",     "parts",  10, 20,  false },
+        { MonsterGroup::Slime,  "슬라임 무리",        120, 20, 8, 0,  "슬라임 부산물"     "parts",  10, 30,  false },
+        { MonsterGroup::Slime,  "불타는 슬라임",   150, 25, 8, 0, "슬라임 부산물",      "parts",  10, 40,  false },
+        { MonsterGroup::Slime,  "검은 슬라임",   180, 30, 10, 0, "슬라임 부산물",      "parts",  10, 50,  false },
+        { MonsterGroup::Slime,  "슬라임 킹",    300, 40, 15, 0, "토벌 증서",     "license",  10, 100, true  },
 
         // Undead 
-        { MonsterGroup::Undead,  "Rotten Crows",  100, 20, 5, 0, "Rotten Flesh",   "parts",  15, 25,  false },
-        { MonsterGroup::Undead,  "Working Dead",  140, 25, 10, 0, "Rotten Flesh",     "parts",  15, 35,  false },
-        { MonsterGroup::Undead,  "Zombies",       170, 30, 10, 0, "Rotten Flesh",   "parts",  15, 45,  false },
-        { MonsterGroup::Undead,  "Skeletons",     200, 35, 12, 0, "Rotten Flesh",       "parts",  15, 55,  false },
-        { MonsterGroup::Undead,  "The Creature",  350, 45, 18, 0, "Dark Heart",     "parts",  90, 120, true  },
+        { MonsterGroup::Undead,  "죽은 새",  100, 20, 5, 0, "언데드 부산물",   "parts",  15, 25,  false },
+        { MonsterGroup::Undead,  "좀비",  140, 25, 10, 0, "언데드 부산물",     "parts",  15, 35,  false },
+        { MonsterGroup::Undead,  "좀비 무리",       170, 30, 10, 0, "언데드 부산물",   "parts",  15, 45,  false },
+        { MonsterGroup::Undead,  "해골",     200, 35, 12, 0, "언데드 부산물",       "parts",  15, 55,  false },
+        { MonsterGroup::Undead,  "크리처",  350, 45, 18, 0, "토벌 증서",     "license",  90, 120, true  },
 
         // Golem 
-        { MonsterGroup::Golem,  "Mud Golem",     130, 18, 15, 0, "Golem Heart",       "parts",  35, 30,  false },
-        { MonsterGroup::Golem,  "Sand Golem",    160, 22, 18, 0, "Golem Heart",    "parts",  35, 40,  false },
-        { MonsterGroup::Golem,  "Wood Golem",    190, 28, 20, 0, "Golem Heart",    "parts",  35, 50,  false },
-        { MonsterGroup::Golem,  "Stone Golem",   230, 35, 25, 0, "Golem Heart",      "parts",  35, 65,  false },
-        { MonsterGroup::Golem,  "The Huge",      400, 50, 30, 0, "Golem Heart",     "parts", 100, 150, true  },
+        { MonsterGroup::Golem,  "진흙 골렘",     130, 18, 15, 0, "골렘의 핵",    "parts",  35, 30,  false },
+        { MonsterGroup::Golem,  "모래 골렘",    160, 22, 18, 0, "골렘의 핵",     "parts",  35, 40,  false },
+        { MonsterGroup::Golem,  "나무 골렘",    190, 28, 20, 0, "골렘의 핵",     "parts",  35, 50,  false },
+        { MonsterGroup::Golem,  "바위 골렘",   230, 35, 25, 0, "골렘의 핵",      "parts",  35, 65,  false },
+        { MonsterGroup::Golem,  "거대 골렘",      400, 50, 30, 0, "토벌 증서",   "license", 100, 150, true  },
+
     };
     return monsters;
 }
