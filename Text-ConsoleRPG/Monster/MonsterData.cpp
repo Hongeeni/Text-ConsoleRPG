a9589ac23c58d98642fba4fe924d874aa09ee8b0
SeongEun-Kwack <144892153+SeongEun-Kwack@users.noreply.github.com>
2026-07-31 12:34:01 +0900

    edit - miss spelling

--- Text-ConsoleRPG/Monster/MonsterData.cpp
+++ Text-ConsoleRPG/Monster/MonsterData.cpp
@@ -15,7 +15,7 @@ vector<MonsterData> MonsterList() {
         { MonsterGroup::Slime, 105, "Slime King",    300, 40, 15, "Slime Jelly",     "parts",  10, 100, true  },
 
         // Undead 
-        { MonsterGroup::Undead, 201, "Rotten Crows",  100, 20, 5,  "Rotten Fleshr",   "parts",  15, 25,  false },
+        { MonsterGroup::Undead, 201, "Rotten Crows",  100, 20, 5,  "Rotten Flesh",   "parts",  15, 25,  false },
         { MonsterGroup::Undead, 202, "Working Dead",  140, 25, 10, "Rotten Flesh",     "parts",  15, 35,  false },
         { MonsterGroup::Undead, 203, "Zombies",       170, 30, 10, "Rotten Flesh",   "parts",  15, 45,  false },
         { MonsterGroup::Undead, 204, "Skeletons",     200, 35, 12, "Rotten Flesh",       "parts",  15, 55,  false },
